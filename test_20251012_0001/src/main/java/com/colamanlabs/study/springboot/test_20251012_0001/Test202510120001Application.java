2b885348baf4efb5e1f3d501bd6406844791e5e0
colamanlabs <colamanlabs@gmail.com>
2025-10-12 21:09:12 +0900

    최초 commit

---
+++ test_20251012_0001/src/main/java/com/colamanlabs/study/springboot/test_20251012_0001/Test202510120001Application.java
@@ -0,0 +1,15 @@
+package com.colamanlabs.study.springboot.test_20251012_0001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test202510120001Application
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Test202510120001Application.class, args);
+    }
+
+}
---
+++ test_20251012_0001/src/test/java/com/colamanlabs/study/springboot/test_20251012_0001/Test202510120001ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.colamanlabs.study.springboot.test_20251012_0001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test202510120001ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
